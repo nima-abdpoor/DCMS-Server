c1f11a2822429a30d4d11b621dac3273c30b13a1
nima abdpoor <nimaabdpoor@gmail.com>
2022-11-27 16:05:26 +0330

    design the config database.

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,12 +1,49 @@
 CREATE TABLE "config"
 (
-    "id"                bigserial PRIMARY KEY,
-    "is_live"           boolean,
-    "sync_type"          varchar,
-    "valid_request_url" varchar,
-    "url_id_first"      varchar,
-    "url_id_second"     varchar,
-    "regex"             varchar,
-    "start_index"       int,
-    "finish_index"      int
+    "id"        bigserial NOT NULL,
+    "uid"       varchar   NOT NULL,
+    "is_live"   boolean   NOT NULL,
+    "sync_type" varchar   NOT NULL,
+    PRIMARY KEY ("id", "uid")
 );
+
+CREATE TABLE "urlFirst"
+(
+    "id"       bigserial PRIMARY KEY NOT NULL,
+    "uid"      varchar               NOT NULL,
+    "url_hash" varchar
+);
+
+CREATE TABLE "urlSecond"
+(
+    "id"           bigserial PRIMARY KEY NOT NULL,
+    "uid"          varchar,
+    "url_hash"     varchar,
+    "regex"        nvarchar,
+    "start_index"  int,
+    "finish_index" int
+);
+
+CREATE TABLE "requestUrl"
+(
+    "id"          bigserial PRIMARY KEY NOT NULL,
+    "uid"         varchar               NOT NULL,
+    "request_url" varchar
+);
+
+CREATE INDEX ON "config" ("uid");
+
+CREATE INDEX ON "urlFirst" ("uid");
+
+CREATE INDEX ON "urlSecond" ("uid");
+
+CREATE INDEX ON "requestUrl" ("uid");
+
+ALTER TABLE "urlFirst"
+    ADD FOREIGN KEY ("uid") REFERENCES "config" ("id");
+
+ALTER TABLE "urlSecond"
+    ADD FOREIGN KEY ("uid") REFERENCES "config" ("id");
+
+ALTER TABLE "requestUrl"
+    ADD FOREIGN KEY ("uid") REFERENCES "config" ("id");
