e42fbe996b6959492de5d9a09765a13e7a59d075
nima abdpoor <nimaabdpoor@gmail.com>
2022-11-28 12:55:40 +0330

    requestUrl table queries generated by sqlc

---
+++ db/query/requestUrl.sql
@@ -0,0 +1,25 @@
+-- name: CreateRequestUrl :one
+INSERT INTO requestUrl (id,
+                        request_url)
+values ($1, $2) RETURNING *;
+
+-- name: GetRequestUrl :one
+SELECT *
+FROM requestUrl
+WHERE id = $1 LIMIT 1;
+
+-- name: ListRequestUrls :many
+SELECT *
+FROM requestUrl
+ORDER BY id LIMIT $1
+OFFSET $2;
+
+-- name: DeleteRequestUrl :exec
+DELETE
+FROM requestUrl
+WHERE id = $1;
+
+-- name: UpdateRequestUrl :one
+UPDATE requestUrl
+set request_url = $2
+WHERE id = $1 RETURNING *;
