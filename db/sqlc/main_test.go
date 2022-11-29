a4b546f9f988f6b1b1cdbe46336a358d71a55064
nima abdpoor <nimaabdpoor@gmail.com>
2022-11-29 11:14:20 +0330

    init test files to connect to database.

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
+	"testing"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5432/DCMS?sslmode=disable"
+)
+
+var testQueries *Queries
+var testDB *sql.DB
+
+func TestMain(m *testing.M) {
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("can not connect to db:", err)
+	}
+	testQueries = New(testDB)
+
+	os.Exit(m.Run())
+}
---
+++ util/SyncTypeModel.go
@@ -0,0 +1,8 @@
+package util
+
+const (
+	Default = "0"
+	Low     = "1"
+	Medium  = "2"
+	High    = "3"
+)
