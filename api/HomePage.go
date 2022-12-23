65760a780f53a1342748326632d5da10f50b9d30
nima-abdpoor <nimaabdpoor@gmail.com>
2022-12-23 18:23:54 +0330

    add home page to load get config file.

---
+++ api/HomePage.go
@@ -0,0 +1,68 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func (server *Server) homePage(ctx *gin.Context) {
+	ctx.HTML(http.StatusOK, "index.html", nil)
+}
+
+func uploadSingleFile(ctx *gin.Context) {
+	file, header, err := ctx.Request.FormFile("image")
+	if err != nil {
+		ctx.String(http.StatusBadRequest, fmt.Sprintf("file err : %s", err.Error()))
+		return
+	}
+
+	fileExt := filepath.Ext(header.Filename)
+	originalFileName := strings.TrimSuffix(filepath.Base(header.Filename), filepath.Ext(header.Filename))
+	filePath := "http://localhost:8080/images/single/" + originalFileName + fileExt
+	out, err := os.Create("public/single/" + originalFileName + fileExt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	data := CatlogNodes{}
+	var byteFile []byte
+	_, err = out.Read(byteFile)
+	err = json.Unmarshal(byteFile, &data)
+	defer out.Close()
+	_, err = io.Copy(out, file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx.JSON(http.StatusOK, gin.H{"filepath": filePath})
+}
+
+func init() {
+	if _, err := os.Stat("public/single"); errors.Is(err, os.ErrNotExist) {
+		err := os.MkdirAll("public/single", os.ModePerm)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+	if _, err := os.Stat("public/multiple"); errors.Is(err, os.ErrNotExist) {
+		err := os.MkdirAll("public/multiple", os.ModePerm)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+type CatlogNodes struct {
+	CatlogNodes []Catlog `json:"catlog_nodes"`
+}
+
+type Catlog struct {
+	Product_id string `json: "product_id"`
+	Quantity   int    `json: "quantity"`
+}
