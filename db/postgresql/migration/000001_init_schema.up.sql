d582449ea71db102e26368c9bc284177402f042e
nima-abdpoor <nimaabdpoor@gmail.com>
2023-01-13 20:17:27 +0330

    create customer table.

--- db/postgresql/migration/000001_init_schema.up.sql
+++ db/postgresql/migration/000001_init_schema.up.sql
@@ -1,18 +1,30 @@
+CREATE TABLE "customer"
+(
+    "id"           bigserial PRIMARY KEY,
+    "user"         varchar NOT NULL,
+    "password"     varchar NOT NULL,
+    "name"         varchar NOT NULL,
+    "email"        varchar NOT NULL,
+    "package_name" varchar NOT NULL,
+    "sdk_uuid"     varchar NOT NULL,
+    "secret_key"   varchar NOT NULL
+);
+
 CREATE TABLE "config"
 (
-    "id"                       bigserial PRIMARY KEY,
-    "is_live"                  boolean   NOT NULL,
-    "save_request"             boolean   NOT NULL,
-    "save_response"            boolean   NOT NULL,
-    "save_error"               boolean   NOT NULL,
-    "save_success"             boolean   NOT NULL,
-    "network_type"             varchar   NOT NULL,
-    "repeat_interval"          bigserial NOT NULL,
+    "id"                        bigserial PRIMARY KEY,
+    "is_live"                   boolean   NOT NULL,
+    "save_request"              boolean   NOT NULL,
+    "save_response"             boolean   NOT NULL,
+    "save_error"                boolean   NOT NULL,
+    "save_success"              boolean   NOT NULL,
+    "network_type"              varchar   NOT NULL,
+    "repeat_interval"           bigserial NOT NULL,
     "repeat_interval_time_unit" varchar   NOT NULL,
     "requires_battery_not_low"  boolean   NOT NULL,
     "requires_storage_not_low"  boolean   NOT NULL,
-    "requires_charging"        boolean   NOT NULL,
-    "requires_device_idl"      boolean   NOT NULL
+    "requires_charging"         boolean   NOT NULL,
+    "requires_device_idl"       boolean   NOT NULL
 );
 
 CREATE TABLE "urlfirst"
